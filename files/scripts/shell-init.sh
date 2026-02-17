60ca6d254526ac76cd303728f6cc5a37ae405cb5
John Holt <jholtiv@gmail.com>
2026-02-17 12:23:44 +0700

    add nushell repo

--- files/scripts/shell-init.sh
+++ files/scripts/shell-init.sh
@@ -2,3 +2,10 @@
 
 starship init nu > /etc/nushell/starship.nu
 zoxide init nushell > /etc/nushell/zoxide.nu
+echo "[gemfury-nushell]
+name=Gemfury Nushell Repo
+baseurl=https://yum.fury.io/nushell/
+enabled=1
+gpgcheck=0
+gpgkey=https://yum.fury.io/nushell/gpg.key" | sudo tee /etc/yum.repos.d/fury-nushell.repo
+sudo dnf install -y nushell

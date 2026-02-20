fa5cdf1ebea2d0e043c31a0a12298b3d2a790c69
John Holt <jholtiv@gmail.com>
2026-02-20 12:15:19 +0700

    test custom kernel

--- modules/custom-kernel/custom-kernel.sh
+++ modules/custom-kernel/custom-kernel.sh
@@ -21,7 +21,6 @@ log "Starting custom-kernel module..."
 # Read configuration from the first argument ($1) using jq
 KERNEL_TYPE=$(echo "$1" | jq -r '.kernel // "cachyos-lto"')
 INITRAMFS=$(echo "$1" | jq -r '.initramfs // false')
-NVIDIA=$(echo "$1" | jq -r '.nvidia // false')
 SIGNING_KEY=$(echo "$1" | jq -r '.sign.key // ""')
 SIGNING_CERT=$(echo "$1" | jq -r '.sign.cert // ""')
 MOK_PASSWORD=$(echo "$1" | jq -r '.sign.["mok-password"] // ""')
@@ -99,6 +98,18 @@ cachyos-lts-lto)
         kernel-cachyos-lts-lto-devel-matched
     )
     ;;
+cachyos-lts-lto-nvidia)
+    COPR_REPOS=(
+        bieszczaders/kernel-cachyos-lto
+    )
+    KERNEL_PACKAGES=(
+        kernel-cachyos-lts-lto
+        kernel-cachyos-lts-lto-core
+        kernel-cachyos-lts-lto-modules
+        kernel-cachyos-lts-lto-devel-matched
+        kernel-cachyos-lts-lto-nvidia-open
+    )
+    ;;
 cachyos)
     COPR_REPOS=(
         bieszczaders/kernel-cachyos
@@ -108,6 +119,18 @@ cachyos)
         kernel-cachyos-core
         kernel-cachyos-modules
         kernel-cachyos-devel-matched
+    )
+    ;;
+    cachyos-nvidia)
+    COPR_REPOS=(
+        bieszczaders/kernel-cachyos
+    )
+    KERNEL_PACKAGES=(
+        kernel-cachyos
+        kernel-cachyos-core
+        kernel-cachyos-modules
+        kernel-cachyos-devel-matched
+        kernel-cachyos-nvidia-open
     )
     ;;
 cachyos-rt)
@@ -121,6 +144,18 @@ cachyos-rt)
         kernel-cachyos-rt-devel-matched
     )
     ;;
+cachyos-rt-nvidia)
+    COPR_REPOS=(
+        bieszczaders/kernel-cachyos
+    )
+    KERNEL_PACKAGES=(
+        kernel-cachyos-rt
+        kernel-cachyos-rt-core
+        kernel-cachyos-rt-modules
+        kernel-cachyos-rt-devel-matched
+        kernel-cachyos-rt-nvidia-open
+    )
+    ;;
 cachyos-lts)
     COPR_REPOS=(
         bieszczaders/kernel-cachyos
@@ -130,6 +165,18 @@ cachyos-lts)
         kernel-cachyos-lts-core
         kernel-cachyos-lts-modules
         kernel-cachyos-lts-devel-matched
+    )
+    ;;
+cachyos-lts-nvidia)
+    COPR_REPOS=(
+        bieszczaders/kernel-cachyos
+    )
+    KERNEL_PACKAGES=(
+        kernel-cachyos-lts
+        kernel-cachyos-lts-core
+        kernel-cachyos-lts-modules
+        kernel-cachyos-lts-devel-matched
+        kernel-cachyos-lts-nvidia-open
     )
     ;;
 *)
@@ -202,31 +249,6 @@ restore_kernel_install_hooks
 log "Cleaning up custom kernel repos."
 rm -f /etc/yum.repos.d/*copr*
 
-# Install Nvidia if needed
-disable_akmodsbuild() {
-    local AK="/usr/sbin/akmodsbuild"
-    local BAK="${AK}.backup"
-
-    if [[ ! -f "${AK}" ]]; then
-        error "akmodsbuild not found: ${AK}"
-        return 1
-    fi
-
-    cp -a "${AK}" "${BAK}" || return 1
-
-    # remove the problematic block
-    sed -i '/if \[\[ -w \/var \]\] ; then/,/fi/d' "${AK}" || return 1
-}
-
-restore_akmodsbuild() {
-    local AK="/usr/sbin/akmodsbuild"
-    local BAK="${AK}.backup"
-
-    if [[ -f "${BAK}" ]]; then
-        mv -f "${BAK}" "${AK}"
-    fi
-}
-
 log "Temporarily disabling akmodsbuild script for v4l2loopback."
 disable_akmodsbuild || exit 1
 
@@ -263,118 +285,7 @@ rm -f /etc/yum.repos.d/rpmfusion-free*.repo
 log "Restoring akmodsbuild script."
 restore_akmodsbuild
 
-if [[ ${NVIDIA} == true ]]; then
-    log "Enabling Nvidia repositories."
-    curl -fsSL --retry 5 --create-dirs \
-        https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo \
-        -o /etc/yum.repos.d/nvidia-container-toolkit.repo
-    curl -fsSL --retry 5 --create-dirs \
-        https://negativo17.org/repos/fedora-nvidia.repo \
-        -o /etc/yum.repos.d/fedora-nvidia.repo
 
-
-    log "Temporarily disabling akmodsbuild script."
-    disable_akmodsbuild || exit 1
-
-    log "Building and installing Nvidia kernel module packages."
-    dnf install -y --setopt=install_weak_deps=False --setopt=tsflags=noscripts \
-        akmod-nvidia \
-        nvidia-kmod-common \
-        nvidia-modprobe \
-        gcc-c++
-    akmods --force --verbose --kernels "${KERNEL_VERSION}" --kmod "nvidia"
-
-    # akmods always fails with exit 0 so we have to check explicitly
-    FAIL_LOG_GLOB=/var/cache/akmods/nvidia/*-for-${KERNEL_VERSION}.failed.log
-
-    shopt -s nullglob
-    FAIL_LOGS=( ${FAIL_LOG_GLOB} )
-    shopt -u nullglob
-
-    if (( ${#FAIL_LOGS[@]} )); then
-        error "Nvidia akmod build failed"
-        for f in "${FAIL_LOGS[@]}"; do
-            cat "${f}" || log "Failed to read ${f}"
-            log "--------------"
-        done
-        exit 1
-    fi
-
-    log "Restoring akmodsbuild script."
-    restore_akmodsbuild
-
-    log "Installing Nvidia userspace packages."
-    dnf install -y --setopt=skip_unavailable=1 \
-        libva-nvidia-driver \
-        nvidia-driver \
-        nvidia-persistenced \
-        nvidia-settings \
-        nvidia-driver-cuda \
-        libnvidia-cfg \
-        libnvidia-fbc \
-        libnvidia-ml \
-        libnvidia-gpucomp \
-        nvidia-driver-libs.i686 \
-        nvidia-driver-cuda-libs.i686 \
-        libnvidia-fbc.i686 \
-        libnvidia-ml.i686 \
-        libnvidia-gpucomp.i686 \
-        nvidia-container-toolkit
-
-    log "Cleaning Nvidia repositories."
-    rm -f /etc/yum.repos.d/*nvidia*
-
-    log "Installing Nvidia SELinux policy."
-    curl -fsSL --retry 5 --create-dirs \
-        https://raw.githubusercontent.com/NVIDIA/dgx-selinux/master/bin/RHEL9/nvidia-container.pp \
-        -o nvidia-container.pp
-    semodule -i nvidia-container.pp
-    rm -f nvidia-container.pp
-
-    log "Installing Nvidia container toolkit service and preset."
-    install -D -m 0644 /dev/stdin /usr/lib/systemd/system/nvctk-cdi.service <<'EOF'
-[Unit]
-Description=NVIDIA Container Toolkit CDI auto-generation
-ConditionFileIsExecutable=/usr/bin/nvidia-ctk
-ConditionPathExists=!/etc/cdi/nvidia.yaml
-After=local-fs.target
-
-[Service]
-Type=oneshot
-ExecStart=/usr/bin/nvidia-ctk cdi generate --output=/etc/cdi/nvidia.yaml
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-    install -D -m 0644 /dev/stdin /usr/lib/systemd/system-preset/70-nvctk-cdi.preset <<'EOF'
-enable nvctk-cdi.service
-EOF
-
-    log "Setting up Nvidia modules."
-    install -D -m 0644 /dev/stdin /etc/modprobe.d/nvidia.conf <<'EOF'
-blacklist nouveau
-options nouveau modeset=0
-options nvidia-drm modeset=1 fbdev=1
-EOF
-
-    log "Setting up GPU modules for initramfs."
-    install -D -m 0644 /dev/stdin /usr/lib/dracut/dracut.conf.d/99-nvidia.conf <<'EOF'
-# Force the i915 amdgpu nvidia drivers to the ramdisk
-force_drivers+=" i915 amdgpu nvidia nvidia_drm nvidia_modeset nvidia_peermem nvidia_uvm "
-EOF
-
-    log "Injecting Nvidia kernel args"
-    install -D -m 0644 /dev/stdin /usr/lib/bootc/kargs.d/90-nvidia.toml <<'EOF'
-kargs = [
-"rd.driver.blacklist=nouveau",
-"modprobe.blacklist=nouveau",
-"rd.driver.pre=nvidia",
-"nvidia-drm.modeset=1",
-"nvidia-drm.fbdev=1"
-]
-EOF
-fi
 
 # Sign the kernel and modules
 sign_kernel() {
@@ -523,19 +434,6 @@ if [[ ${SECURE_BOOT} == true ]]; then
     log "Kernel was not modified after signing."
 fi
 
-if [[ ${NVIDIA} == true ]]; then
-    DIR="/usr/lib/modules/${KERNEL_VERSION}/extra/nvidia"
 
-    [[ -d "${DIR}" ]] || { error "Missing Nvidia module directory: ${DIR}"; exit 1; }
-
-    for name in nvidia nvidia-{drm,modeset,peermem,uvm}; do
-        if ! compgen -G "${DIR}/${name}.*" > /dev/null; then
-            error "Missing Nvidia module: ${DIR}/${name}.*"
-            exit 1
-        fi
-    done
-
-    log "All Nvidia modules present."
-fi
 
 log "Custom kernel installation complete."

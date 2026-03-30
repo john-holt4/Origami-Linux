f2132937982e819dfa744dfea0f6dd5abc1450b1
shriman <shriman-w@proton.me>
2026-03-30 19:22:14 +0530

    Revert previous changes

--- files/system/etc/profile.d/nix-uutils-workaround.sh
+++ files/system/etc/profile.d/nix-uutils-workaround.sh
@@ -3,7 +3,5 @@
 # Workaround for nix-shell command unable to get uutils path
 # This script can be removed when this is no longer an issue
 
-if [[ "${SHELL}" =~ "/nix/store" ]]; then
-    uu_true() { /usr/bin/uu_true; }
-    uu_echo() { /usr/bin/uu_echo "$@"; }
-fi
+uu_true() { /usr/bin/uu_true; }
+uu_echo() { /usr/bin/uu_echo "$@"; }
